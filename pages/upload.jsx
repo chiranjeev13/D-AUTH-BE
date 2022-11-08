ecbddab5b88a0d00eb438fce7b16fc624fd7679a
Pranshu1902 <pranshu1902@gmail.com>
2022-11-08 10:48:23 +0530

    tempalte

--- pages/upload.jsx
+++ pages/upload.jsx
@@ -2,6 +2,29 @@ import { Button, TextField } from "@mui/material";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import Header from "../Common/Header";
+const axios = require("axios").default;
+
+const options = {
+  method: "POST",
+  url: "https://d7-verify.p.rapidapi.com/verify/v1/otp/send-otp",
+  headers: {
+    "content-type": "application/json",
+    Token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoLWJhY2tlbmQ6YXBwIiwic3ViIjoiMWU1NmVlZWUtMzY2Ny00MjY0LWI1OGMtZmY4NDg5NGIyODk4In0.vGwG-PkvGQN_LZl-R2rb1lk5bd1_0AqQorgXURqVP9E",
+    "X-RapidAPI-Key": "c4aacdd6f5msh971693a8fd7c123p1dba77jsn01c3b4eb154c",
+    "X-RapidAPI-Host": "d7-verify.p.rapidapi.com",
+  },
+  data: '{"originator":"SignOTP","recipient":"+919082871739","content":"OTP verification code is: {}","expiry":"600","data_coding":"text"}',
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
 
 export default function RouteName() {
   const [name, setName] = useState("");
@@ -9,6 +32,36 @@ export default function RouteName() {
   const [number, setNumber] = useState();
   const [otpSent, setOtpSent] = useState(false);
   const [otp, setOtp] = useState("");
+
+  // send OTP API
+  let options = {
+    method: "POST",
+    url: "https://d7-verify.p.rapidapi.com/verify/v1/otp/send-otp",
+    headers: {
+      "content-type": "application/json",
+      Token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoLWJhY2tlbmQ6YXBwIiwic3ViIjoiMWU1NmVlZWUtMzY2Ny00MjY0LWI1OGMtZmY4NDg5NGIyODk4In0.vGwG-PkvGQN_LZl-R2rb1lk5bd1_0AqQorgXURqVP9E",
+      "X-RapidAPI-Key": "c4aacdd6f5msh971693a8fd7c123p1dba77jsn01c3b4eb154c",
+      "X-RapidAPI-Host": "d7-verify.p.rapidapi.com",
+    },
+    data: '{"originator":"SignOTP","recipient":"+919082871739","content":"OTP verification code is: {}","expiry":"600","data_coding":"text"}',
+  };
+
+  async function handleSubmit() {
+    // setting the data
+    const data = `{"originator":"SignOTP","recipient":"+91${number}","content":"OTP verification code is: {}","expiry":"600","data_coding":"text"}`;
+    options[data] = data;
+
+    // posting the request
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
 
   const { asPath } = useRouter();
   return (
@@ -53,7 +106,10 @@ export default function RouteName() {
               fullWidth
               color="secondary"
               disabled={otpSent}
-              onClick={() => setOtpSent(true)}
+              onClick={() => {
+                setOtpSent(true);
+                handleSubmit();
+              }}
               className="text-purple-800 hover:text-white md:w-auto w-full mt-4"
             >
               Send OTP

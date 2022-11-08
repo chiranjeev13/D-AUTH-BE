9911850d7df74e9450be064567421e27f6dd7e9b
Pranshu1902 <pranshu1902@gmail.com>
2022-11-08 10:51:56 +0530

    integration

--- pages/upload.jsx
+++ pages/upload.jsx
@@ -2,6 +2,8 @@ import { Button, TextField } from "@mui/material";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import Header from "../Common/Header";
+import { abi, contractAddress } from "../constants";
+import { useMoralis, useWeb3Contract } from "react-moralis";
 
 export default function RouteName() {
   const [name, setName] = useState("");
@@ -10,7 +12,39 @@ export default function RouteName() {
   const [otpSent, setOtpSent] = useState(false);
   const [otp, setOtp] = useState("");
 
+  const DAUTHAddress =
+    chainId in contractAddresses ? contractAddresses[chainId][0] : null;
+
   const { asPath } = useRouter();
+
+  const {
+    runContractFunction: mint,
+    data: enterTxResponse,
+    isLoading,
+    isFetching,
+  } = useWeb3Contract({
+    abi: abi,
+    contractAddress: DAUTHAddress,
+    functionName: "mint",
+    params: {
+      args: [1],
+    },
+  });
+  const { runContractFunction: getVerifiedstatus } = useWeb3Contract({
+    abi: abi,
+    contractAddress: DAUTHAddress, // specify the networkId
+    functionName: "getVerifiedstatus",
+    params: {},
+  });
+
+  const { runContractFunction: Verifier } = useWeb3Contract({
+    abi: abi,
+    contractAddress: DAUTHAddress, // specify the networkId
+    functionName: "Verifier",
+    params: {
+      args: [tp],
+    },
+  });
   return (
     <div className="bg-white text-blue-500 min-h-screen">
       <Header />

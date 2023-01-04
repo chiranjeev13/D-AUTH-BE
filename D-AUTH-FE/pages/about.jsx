32231418230550d976d396c5360e3f2372f45507
chiranjeev13 <chiranjeevmishra13@gmail.com>
2023-01-05 00:14:09 +0530

    added styling in about page

--- D-AUTH-FE/pages/about.jsx
+++ D-AUTH-FE/pages/about.jsx
@@ -1,30 +1,32 @@
 import { useRouter } from "next/router";
 import Header from "../Common/Header";
-
 export default function RouteName() {
   const { asPath } = useRouter();
   return (
     <div className="bg-white text-blue-500 min-h-screen ">
       <Header />
       <div>
-        <h5>Hi I am Chiranjeev Mishra,an aspiring Blockchain Developer</h5>
+        <h5>Hi I am Chiranjeev Mishra, an aspiring Blockchain Developer</h5>
+
         <p>
-          This project aims for creating a web3 App which authenticates your
+          This project aims for creating a web-3 App which authenticates your
           web2 financial details like your AADHAR,PAN etc details then mint you
           an NFT after verification.
         </p>
       </div>
-      <div className="text-black flex gap-2">
-        <p>Connect with me at</p>
-        <a className="hover:underline" href="https://github.com/chiranjeev13">
-          GITHUB
-        </a>
-        <a
-          className="hover:underline"
-          href="https://www.linkedin.com/in/chiranjeev-mishra-8b8008205/"
-        >
-          LINKEDIN
-        </a>
+      <div className="absolute bottom-2">
+        <div className="bg-black text-white flex gap-2">
+          <p>Connect with me at</p>
+          <a className="hover:underline" href="https://github.com/chiranjeev13">
+            GITHUB
+          </a>
+          <a
+            className="hover:underline"
+            href="https://www.linkedin.com/in/chiranjeev-mishra-8b8008205/"
+          >
+            LINKEDIN
+          </a>
+        </div>
       </div>
     </div>
   );
